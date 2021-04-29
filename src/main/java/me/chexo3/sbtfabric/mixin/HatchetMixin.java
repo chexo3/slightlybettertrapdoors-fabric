068219c8f0ddf7cb1fbc34b294cd26e3d1db5778
chexo3 <10382225+chexo3@users.noreply.github.com>
2021-04-29 14:44:38 -0400

    Okay, _now_ there should be a working HatchetMixin. The TrapdoorMixin is not finished and will not load. Do not build this, it only implements half the functionality.

---
+++ src/main/java/me/chexo3/sbtfabric/mixin/HatchetMixin.java
@@ -0,0 +1,22 @@
+package me.chexo3.sbtfabric.mixin;
+
+import net.minecraft.block.BlockBase;
+import net.minecraft.item.tool.Hatchet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Arrays;
+
+@Mixin(Hatchet.class)
+public class HatchetMixin {
+	@Shadow
+	private static BlockBase[] effectiveBlocks;
+	@Inject(method = "<clinit>", at = @At("RETURN"))
+	private static void addToArray(CallbackInfo ci) {
+		effectiveBlocks = Arrays.copyOf(effectiveBlocks, effectiveBlocks.length + 1);
+		effectiveBlocks[effectiveBlocks.length - 1] = BlockBase.TRAPDOOR;
+	}
+}
---
+++ src/main/java/me/chexo3/sbtfabric/mixin/TrapdoorMixin.java
@@ -0,0 +1,9 @@
+package me.chexo3.sbtfabric.mixin;
+
+import net.minecraft.block.Trapdoor;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Trapdoor.class)
+public class TrapdoorMixin {
+
+}
